2c43e2574c1d3b37c11916f7b16894ecec67a1e3
Parth Bhavsar <96877042+parth23704@users.noreply.github.com>
2025-08-22 19:25:16 +1000

    Delete Tutorial-3 directory

--- Tutorial-3/Python Files/Version_2.py
+++
@@ -1,8 +0,0 @@
-def seatbelt_alarm(driv, pass_, ign, belt_d_bar, belt_p_bar):
-    if ign == 1 and ((driv == 1 and belt_d_bar == 1) or (pass_ == 1 and belt_p_bar == 1)):
-        return 0  # ON
-    else:
-        return 1  # OFF
-
-# Test
-print(seatbelt_alarm(1, 0, 1, 1, 0))  # Should be 0
--- Tutorial-3/Readme.md
+++
@@ -1,5 +0,0 @@
-I've Created Two Folders which are:
-- Python Files
-- Solutions
-
-In python Files I've Uploaded Version 1 and Version 2 of the python code, and In the Solutions Folder I've Put all the Word files which contains All soltuions, Steps (1-5) and part-3 AI Integration in it.
